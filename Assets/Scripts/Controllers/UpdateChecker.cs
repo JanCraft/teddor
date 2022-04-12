26d034892361d3f6e57d21abdf649db60f7ed850
JanCraft <jancraft888@gmail.com>
2022-04-12 17:33:22 +0200

    Maintenance
    - Maintenance is now checked when opening the game.

--- Assets/Scripts/Controllers/UpdateChecker.cs
+++ Assets/Scripts/Controllers/UpdateChecker.cs
@@ -11,12 +11,25 @@ public class UpdateChecker : MonoBehaviour {
     public UnityEvent onUpdateFound;
     public UnityEvent onRequestFailed;
     public UnityEvent onChannelMismatch;
+    public UnityEvent onMaintenance;
 
     IEnumerator Start() {
         TranslateKey.Init();
         yield return new WaitForSecondsRealtime(delay);
         string gamedefstr = File.ReadAllText(Application.streamingAssetsPath + "/gamedef.json");
         GameDef gamedef = JsonUtility.FromJson<GameDef>(gamedefstr);
+
+        string maintenanceurl = "https://raw.githubusercontent.com/jdev-com-es/jdev.com.es/main/maintenance.json";
+        UnityWebRequest mwww = UnityWebRequest.Get(maintenanceurl);
+        yield return mwww.SendWebRequest();
+        var mobj = TinyJson.JSONParser.FromJson<Maintenance>(mwww.downloadHandler.text);
+        if (mobj.enabled) {
+            onMaintenance.Invoke();
+            yield break;
+        } else if (mobj.start >= (int) System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1)).TotalSeconds) {
+            onMaintenance.Invoke();
+            yield break;
+        }
 
         string checkurl = "https://raw.githubusercontent.com/JanCraft/teddor/" + GetGitBranch(gamedef.channel) + "/Assets/StreamingAssets/gamedef.json";
 
@@ -55,6 +68,14 @@ public class GameDef {
     public string channel;
     public int version;
     public string version_str;
+}
+
+[System.Serializable]
+public class Maintenance {
+    public bool enabled;
+    public string reason;
+    public long start;
+    public long end;
 }
 
 public enum TeddorBuildChannel {
